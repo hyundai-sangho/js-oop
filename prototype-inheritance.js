669858fa780348c1a43793e07dc12ff6eca6b27a
조상호 <hyundai_sangho@naver.com>
2022-09-09 23:54:38 +0900

    객체 상속의 사용

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -9,3 +9,34 @@ console.log("subObj.subVal => " + subObj.subVal);
 console.log("subObj.superVal => " + subObj.superVal);
 subObj.superVal = "sub";
 console.log("superObj.superVal =>", superObj.superVal);
+
+kim = {
+  name: "kim",
+  first: 10,
+  second: 20,
+  sum: function () {
+    return this.first + this.second;
+  },
+};
+
+let lee = Object.create(kim);
+lee.name = "lee";
+lee.first = 10;
+lee.second = 10;
+lee.avg = function () {
+  return (this.first + this.second) / 2;
+};
+
+// lee = {
+//   name: "lee",
+//   first: 10,
+//   second: 10,
+//   avg: function () {
+//     return (this.first + this.second) / 2;
+//   },
+// };
+
+// lee.__proto__ = kim;
+
+console.log("lee.sum() ", kim.sum());
+console.log("lee.avg() ", lee.avg());
