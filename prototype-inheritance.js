00d5276ef525b2202db4d2bfe19d6e1ea04e4c2b
조상호 <hyundai_sangho@naver.com>
2022-09-09 23:40:21 +0900

    Object.create()

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,6 +1,9 @@
 let superObj = { superVal: "super" };
-let subObj = { subVal: "sub" };
-subObj.__proto__ = superObj; // prototype, __proto__
+// let subObj = { subVal: "sub" };
+// subObj.__proto__ = superObj; // prototype, __proto__
+let subObj = Object.create(superObj);
+subObj.subVal = "sub";
+debugger;
 
 console.log("subObj.subVal => " + subObj.subVal);
 console.log("subObj.superVal => " + subObj.superVal);
